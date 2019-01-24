14a0dd12e87a06bcecdd6cec96add66a3c0ffe0e
Monaca, Francesco <francesco.monaca@accenture.com>
2019-01-24 17:02:40 +0100

    test

---
+++ src/TestC.java
@@ -0,0 +1,18 @@
+/**
+ * Test per git
+ */
+
+/**
+ * @author francesco.monaca
+ *
+ */
+public class TestC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Ciao");
+	}
+
+}
